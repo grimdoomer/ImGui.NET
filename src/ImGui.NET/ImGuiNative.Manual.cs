1cb6447502144e20b9559283dc14df3927ff844a
rmiceli <ryan.miceli@gmail.com>
2022-08-25 22:11:07 -0700

    - Added support for PlatformIO callbacks

--- src/ImGui.NET/ImGuiNative.Manual.cs
+++ src/ImGui.NET/ImGuiNative.Manual.cs
@@ -14,5 +14,18 @@ namespace ImGuiNET
 
         [DllImport("cimgui", CallingConvention = CallingConvention.Cdecl)]
         public static extern void igScrollToBringRectIntoView();
+
+        [DllImport("cimgui", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ImGuiPlatformIO_Monitors_Resize(int size);
+
+        [DllImport("cimgui", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ImGuiPlatformIO_Monitors_PushBack(ImGuiPlatformMonitor monitor);
+
+        [DllImport("cimgui", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ImGuiPlatformIO_Monitors_PushFront(ImGuiPlatformMonitor monitor);
+
+
+        [DllImport("cimgui", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ImGuiPlatformIO_SetCallback(int callback, IntPtr funcptr);
     }
 }
